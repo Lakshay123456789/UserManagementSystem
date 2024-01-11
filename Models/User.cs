d291f21d6d6d0d96dac74757b164a944f0e13725
[lakshay kamboj] <lakshaykamboj051@gmail.com>
2024-01-11 19:07:30 +0530

    ChangeInto User Model

--- Models/User.cs
+++ Models/User.cs
@@ -13,13 +13,9 @@ namespace Models
 
         public string? FirstName { get; set; }
 
-
-
         public string? LastName { get; set; }
 
-
         public string? Contact { get; set; }
-
 
         public string? Email { get; set; }
 
